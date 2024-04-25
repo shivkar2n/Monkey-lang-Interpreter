b12c67bc2fcdaa6abcf45c580e15dee3f5537d87
Shiv Karthik <shivkar2n@gmail.com>
2024-04-25 21:45:08 +0530

    Fixed: Function call scope

--- src/evaluator/src/evaluator.cpp
+++ src/evaluator/src/evaluator.cpp
@@ -1,19 +1,15 @@
 #include "evaluator.hpp"
 
 Object *Evaluator::eval_program(std::vector<Statement *> statements,
-                                Environment *env)
-{
+                                Environment *env) {
   Object *result;
 
-  for (auto statement : statements)
-  {
-    switch (statement->type)
-    {
+  for (auto statement : statements) {
+    switch (statement->type) {
     case LETSTATEMENT:
       return eval(statement, env);
 
-    case RETURNSTATEMENT:
-    {
+    case RETURNSTATEMENT: {
       result = new Object;
       auto value = eval(((ReturnStatement *)statement)->val, env);
       result->type = RETURNVAL;
@@ -21,17 +17,14 @@ Object *Evaluator::eval_program(std::vector<Statement *> statements,
       return result;
     }
 
-    case EXPRSTATEMENT:
-    {
+    case EXPRSTATEMENT: {
       result = eval(((ExpressionStatement *)statement)->exp, env);
       break;
     }
     }
 
-    if (result->type != UNINTITIALIZED)
-    {
-      if (result->type == ERROR || result->type == RETURNVAL)
-      {
+    if (result->type != UNINTITIALIZED) {
+      if (result->type == ERROR || result->type == RETURNVAL) {
         return result;
       }
     }
@@ -39,20 +32,14 @@ Object *Evaluator::eval_program(std::vector<Statement *> statements,
   return result;
 }
 
-Object *Evaluator::eval_prefix_expression(AstNode *node, Object *right)
-{
+Object *Evaluator::eval_prefix_expression(AstNode *node, Object *right) {
   auto op = ((PrefixExpression *)(node))->op;
 
-  if (op == "!")
-  {
+  if (op == "!") {
     return eval_boolean_operator(right);
-  }
-  else if (op == "-")
-  {
+  } else if (op == "-") {
     return eval_minus_operator(right);
-  }
-  else
-  {
+  } else {
     int value = right->integerLiteral->value;
     std::string msg = "Unknown operator: " + op + " " + std::to_string(value);
     Object *result;
@@ -64,19 +51,13 @@ Object *Evaluator::eval_prefix_expression(AstNode *node, Object *right)
 }
 
 Object *Evaluator::eval_infix_expression(AstNode *node, Object *left,
-                                         Object *right)
-{
+                                         Object *right) {
 
-  if (left->type == INTEGER && right->type == INTEGER)
-  {
+  if (left->type == INTEGER && right->type == INTEGER) {
     return eval_integer_infix_operator(node, left, right);
-  }
-  else if (left->type == BOOLEAN && right->type == BOOLEAN)
-  {
+  } else if (left->type == BOOLEAN && right->type == BOOLEAN) {
     return eval_boolean_infix_operator(node, left, right);
-  }
-  else if (left->type != right->type)
-  {
+  } else if (left->type != right->type) {
     Object *result;
     auto leftType = left->get_type();
     auto rightType = right->get_type();
@@ -89,79 +70,55 @@ Object *Evaluator::eval_infix_expression(AstNode *node, Object *left,
 }
 
 Object *Evaluator::eval_integer_infix_operator(AstNode *node, Object *left,
-                                               Object *right)
-{
+                                               Object *right) {
   Object *result = new Object;
   auto op = ((InfixExpression *)(node))->op;
   int leftValue = left->integerLiteral->value;
   int rightValue = right->integerLiteral->value;
 
-  if (op == "+")
-  {
+  if (op == "+") {
     int value = (leftValue + rightValue);
     ((Integer *)node)->value = value;
     result->type = INTEGER;
     result->integerLiteral =
         new IntegerLiteral{((Integer *)node)->value = (leftValue + rightValue)};
-  }
-  else if (op == "-")
-  {
+  } else if (op == "-") {
     int value = (leftValue - rightValue);
     ((Integer *)node)->value = value;
     result->type = INTEGER;
-    result->integerLiteral =
-        new IntegerLiteral{value};
-  }
-  else if (op == "*")
-  {
+    result->integerLiteral = new IntegerLiteral{value};
+  } else if (op == "*") {
     int value = (leftValue * rightValue);
     ((Integer *)node)->value = value;
     result->type = INTEGER;
-    result->integerLiteral =
-        new IntegerLiteral{value};
-  }
-  else if (op == "/")
-  {
+    result->integerLiteral = new IntegerLiteral{value};
+  } else if (op == "/") {
     int value = (leftValue / rightValue);
     ((Integer *)node)->value = value;
     result->type = INTEGER;
     result->integerLiteral =
         new IntegerLiteral{((Integer *)node)->value = (leftValue / rightValue)};
-  }
-  else if (op == "<")
-  {
+  } else if (op == "<") {
     bool value = (leftValue < rightValue);
     ((Boolean *)node)->value = value;
     result->type = BOOLEAN;
-    result->booleanLiteral =
-        new BooleanLiteral{value};
-  }
-  else if (op == ">")
-  {
+    result->booleanLiteral = new BooleanLiteral{value};
+  } else if (op == ">") {
     bool value = (leftValue > rightValue);
     ((Boolean *)node)->value = value;
     result->type = BOOLEAN;
-    result->booleanLiteral =
-        new BooleanLiteral{value};
-  }
-  else if (op == "==")
-  {
+    result->booleanLiteral = new BooleanLiteral{value};
+  } else if (op == "==") {
     bool value = (leftValue == rightValue);
     ((Boolean *)node)->value = value;
     result->type = BOOLEAN;
-    result->booleanLiteral =
-        new BooleanLiteral{value};
-  }
-  else if (op == "!=")
-  {
+    result->booleanLiteral = new BooleanLiteral{value};
+  } else if (op == "!=") {
     bool value = (leftValue != rightValue);
     ((Boolean *)node)->value = value;
     result->type = BOOLEAN;
-    result->booleanLiteral =
-        new BooleanLiteral{value};
-  }
-  else
-  {
+    result->booleanLiteral = new BooleanLiteral{value};
+  } else {
     std::string msg = "Unknown operator: " + op + "\n";
     result->type = ERROR;
     result->error = new Error{msg};
@@ -170,32 +127,26 @@ Object *Evaluator::eval_integer_infix_operator(AstNode *node, Object *left,
 }
 
 Object *Evaluator::eval_boolean_infix_operator(AstNode *node, Object *left,
-                                               Object *right)
-{
+                                               Object *right) {
   Object *result = new Object;
   auto op = ((InfixExpression *)node)->op;
 
   int leftValue = left->booleanLiteral->value;
   int rightValue = right->booleanLiteral->value;
 
-  if (op == "==")
-  {
+  if (op == "==") {
     bool value = (leftValue == rightValue);
     ((Boolean *)node)->value = value;
     result->type = BOOLEAN;
     result->booleanLiteral = new BooleanLiteral{value};
     return result;
-  }
-  else if (op == "!=")
-  {
+  } else if (op == "!=") {
     bool value = (leftValue != rightValue);
     ((Boolean *)node)->value = value;
     result->type = BOOLEAN;
     result->booleanLiteral = new BooleanLiteral{value};
     return result;
-  }
-  else
-  {
+  } else {
     std::string msg = "Unknown operator: " + op + "\n";
     result->type = ERROR;
     result->error = new Error{msg};
@@ -203,32 +154,26 @@ Object *Evaluator::eval_boolean_infix_operator(AstNode *node, Object *left,
   }
 }
 
-Object *Evaluator ::eval_boolean_operator(Object *right)
-{
+Object *Evaluator ::eval_boolean_operator(Object *right) {
   Object *result = new Object;
   result->type = BOOLEAN;
 
-  switch (right->integerLiteral->value)
-  {
-  case 0:
-  {
+  switch (right->integerLiteral->value) {
+  case 0: {
     result->booleanLiteral = new BooleanLiteral{true};
     return result;
   }
 
-  default:
-  {
+  default: {
     result->booleanLiteral = new BooleanLiteral{false};
     return result;
   }
   }
 }
 
-Object *Evaluator ::eval_minus_operator(Object *right)
-{
+Object *Evaluator ::eval_minus_operator(Object *right) {
   Object *result = new Object;
-  if (right->type != INTEGER)
-  {
+  if (right->type != INTEGER) {
     std::string msg = "Unknown operator: -" + right->get_type() + "\n";
     result->type = ERROR;
     result->error = new Error{msg};
@@ -242,11 +187,9 @@ Object *Evaluator ::eval_minus_operator(Object *right)
 
 std::vector<Object *>
 Evaluator ::eval_expressions(std::vector<Expression *> expressions,
-                             Environment *env)
-{
+                             Environment *env) {
   std::vector<Object *> result;
-  for (auto &expression : expressions)
-  {
+  for (auto &expression : expressions) {
     auto evaluated = eval(expression, env);
     if (is_error(evaluated))
       return std::vector<Object *>{evaluated};
@@ -255,31 +198,24 @@ Evaluator ::eval_expressions(std::vector<Expression *> expressions,
   return result;
 }
 
-Object *Evaluator::eval(AstNode *node, Environment *env)
-{
-  switch (node->nodeType)
-  {
-  case PROGRAM:
-  {
+Object *Evaluator::eval(AstNode *node, Environment *env) {
+  switch (node->nodeType) {
+  case PROGRAM: {
     auto programNode = (Program *)node;
     return eval_program(programNode->statements, env);
   }
 
-  case EXPRESSION:
-  {
+  case EXPRESSION: {
     Expression *exprNode = (Expression *)node;
-    switch (exprNode->expressionType)
-    {
+    switch (exprNode->expressionType) {
 
-    case IDENTIFIEREXPR:
-    {
+    case IDENTIFIEREXPR: {
       auto identifier = ((Identifier *)node);
       auto id = ((Identifier *)node)->id;
       return env->get(id);
     }
 
-    case BOOLEXPR:
-    {
+    case BOOLEXPR: {
       Object *result = new Object;
       bool value = ((Boolean *)exprNode)->value;
       result->type = BOOLEAN;
@@ -287,8 +223,7 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       return result;
     }
 
-    case INTEGEREXPR:
-    {
+    case INTEGEREXPR: {
       Object *result = new Object;
       int value = ((Integer *)exprNode)->value;
       result->type = INTEGER;
@@ -296,8 +231,7 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       return result;
     }
 
-    case PREFIXEXPR:
-    {
+    case PREFIXEXPR: {
       auto expr = (((Expression *)(node))->right);
       auto right = eval(expr, env);
       if (is_error(right))
@@ -305,8 +239,7 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       return eval_prefix_expression(node, right);
     }
 
-    case INFIXEXPR:
-    {
+    case INFIXEXPR: {
       auto expr = (((Expression *)(node))->left);
       auto left = eval(expr, env);
       if (is_error(left))
@@ -320,24 +253,18 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       return eval_infix_expression(node, left, right);
     }
 
-    case IFEXPR:
-    {
+    case IFEXPR: {
       auto expr = (IfExpression *)node;
       auto condition = eval(expr->cond, env);
 
       if (is_error(condition))
         return condition;
 
-      if (is_truthy(condition))
-      {
+      if (is_truthy(condition)) {
         return eval(expr->conseq, env);
-      }
-      else if (expr->altern != NULL)
-      {
+      } else if (expr->altern != NULL) {
         return eval(expr->altern, env);
-      }
-      else
-      {
+      } else {
         Object *result = new Object;
         result->type = NULLVAL;
         result->nullLiteral = new NullLiteral();
@@ -345,8 +272,7 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       }
     }
 
-    case FNEXPR:
-    {
+    case FNEXPR: {
       auto expr = (FunctionLiteral *)node;
       auto body = expr->body;
       auto parameters = expr->parameters;
@@ -358,8 +284,7 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       return result;
     }
 
-    case CALLEXPR:
-    {
+    case CALLEXPR: {
       auto callExpression = (CallExpression *)node;
       auto function = eval(callExpression->function, env);
       if (is_error(function))
@@ -370,15 +295,12 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
     }
   }
 
-  case STATEMENT:
-  {
+  case STATEMENT: {
     auto statementNode = (Statement *)node;
 
-    switch (statementNode->type)
-    {
+    switch (statementNode->type) {
 
-    case LETSTATEMENT:
-    {
+    case LETSTATEMENT: {
       auto identifierExpression =
           (Identifier *)((LetStatement *)statementNode)->id;
       auto id = identifierExpression->id;
@@ -389,14 +311,12 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
       return env->set(id, value);
     }
 
-    case BLOCKSTATEMENT:
-    {
+    case BLOCKSTATEMENT: {
       auto statements = ((BlockStatement *)statementNode)->statements;
       return eval_block_statements(statements, env);
     }
 
-    case RETURNSTATEMENT:
-    {
+    case RETURNSTATEMENT: {
       auto valueExpression = ((ReturnStatement *)statementNode)->val;
       auto value = eval(valueExpression, env);
       if (is_error(value))
@@ -413,21 +333,17 @@ Object *Evaluator::eval(AstNode *node, Environment *env)
 }
 
 Object *Evaluator::eval_block_statements(std::vector<Statement *> statements,
-                                         Environment *env)
-{
-  Object *result;
-  for (auto statement : statements)
-  {
+                                         Environment *env) {
+  Object *result = NULL;
+  for (auto statement : statements) {
     auto statementNode = (AstNode *)statement;
-    switch (statement->type)
-    {
-    case LETSTATEMENT:
-    {
-      return eval(statement, env);
+    switch (statement->type) {
+    case LETSTATEMENT: {
+      eval(statement, env);
+      break;
     }
 
-    case RETURNSTATEMENT:
-    {
+    case RETURNSTATEMENT: {
       auto value = eval(((ReturnStatement *)statementNode)->val, env);
       result = new Object;
       result->type = RETURNVAL;
@@ -435,15 +351,13 @@ Object *Evaluator::eval_block_statements(std::vector<Statement *> statements,
       return result;
     }
 
-    case EXPRSTATEMENT:
-    {
+    case EXPRSTATEMENT: {
       result = eval(((ExpressionStatement *)statementNode)->exp, env);
       break;
     }
     }
 
-    if (result->type != UNINTITIALIZED)
-    {
+    if (result != NULL && result->type != UNINTITIALIZED) {
       if (result->type == ERROR || result->type == RETURNVAL)
         return result;
     }
@@ -452,8 +366,7 @@ Object *Evaluator::eval_block_statements(std::vector<Statement *> statements,
 }
 
 Object *Evaluator ::apply_function(Object *object,
-                                   std::vector<Object *> arguements)
-{
+                                   std::vector<Object *> arguements) {
 
   Environment *extendedEnv = extend_function_env(object, arguements);
 
@@ -468,33 +381,24 @@ Object *Evaluator ::apply_function(Object *object,
 }
 
 Environment *Evaluator ::extend_function_env(Object *object,
-                                             std::vector<Object *> arguements)
-{
+                                             std::vector<Object *> arguements) {
   Environment *env = new Environment(object->function->env);
   return env;
 }
 
-Object *Evaluator::unwrap_return_value(Object *result)
-{
-  if (result->type == RETURNVAL)
-  {
+Object *Evaluator::unwrap_return_value(Object *result) {
+  if (result->type == RETURNVAL) {
     return result->returnLiteral->value;
   }
   return result;
 }
 
-bool Evaluator ::is_truthy(Object *condition)
-{
-  if (condition->type == BOOLEAN)
-  {
+bool Evaluator ::is_truthy(Object *condition) {
+  if (condition->type == BOOLEAN) {
     return condition->booleanLiteral->value;
-  }
-  else if (condition->type == NULLVAL)
-  {
+  } else if (condition->type == NULLVAL) {
     return false;
-  }
-  else
-  {
+  } else {
     return true;
   }
 }
